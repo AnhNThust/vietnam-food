cb3ef49ceb533281ec80478e55fc62dc721876a9
tuananh <anhntlinh@gmail.com>
2026-04-18 21:28:48 +0700

    second commit

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#9A0021',
+        secondary: '#feb700'
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif']
+      }
+    }
+  }
+}
